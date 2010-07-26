f58b88f8e9f5b8adf710adee2628d9291085d69b
Brian Arnold <brianarn@gmail.com>
2010-07-26 17:05:52 -0600

    Initial add

---
+++ init.js
@@ -0,0 +1,6 @@
+dojo.provide("ColorSlider.init");
+
+dojo.addOnLoad(function(){
+	console.log("init()");
+	dojo.byId("demoContainer").innerHTML="<h1>Hello World</h1>";
+});
---
+++ layer.js
@@ -0,0 +1,7 @@
+dojo.provide("ColorSlider.layer");
+
+// register module path for this Demo
+dojo.registerModulePath("ColorSlider","../../ColorSlider");
+
+/* put all of the dojo.requires for your demo here */
+dojo.require("ColorSlider.init");
